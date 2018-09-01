e8986605cda21e057a3162b648b92aa77a7aafdb
Kuntal Majumder <hellozee@disroot.org>
2018-09-01 11:23:51 +0530

    Updated camera to accomodate a separate ray tracer module

--- src/camera/camera.c
+++ src/camera/camera.c
@@ -1,12 +1,15 @@
 #include "camera.h"
 
 c_camera 
-new_camera(c_vector pos, c_vector dir, c_vector rht, c_vector dwn)
+new_camera(c_vector pos, c_vector dir, c_vector rht, c_vector dwn,
+           unsigned int w, unsigned int h)
 {
     c_camera c;
     c.position = pos;
     c.direction = dir;
     c.right = rht;
     c.down = dwn;
+    c.width = w;
+    c.height = h;
     return c;
 }
--- src/camera/camera.h
+++ src/camera/camera.h
@@ -5,8 +5,10 @@
 
 typedef struct{
     c_vector position, direction, right, down;
+    unsigned int width, height;
 }c_camera;
 
-c_camera new_camera(c_vector pos, c_vector dir, c_vector rht, c_vector dwn);
+c_camera new_camera(c_vector pos, c_vector dir, c_vector rht, c_vector dwn,
+                    unsigned int w, unsigned int h);
 
 #endif
