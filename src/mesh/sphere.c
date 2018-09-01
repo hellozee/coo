c03813e2e83eadbd9364a867de1ed5538d876f40
Kuntal Majumder <hellozee@disroot.org>
2018-09-01 10:40:04 +0530

    Implemented intersection finding algorithm for sphere

--- src/mesh/sphere.c
+++ src/mesh/sphere.c
@@ -23,10 +23,25 @@ sphere_find_intersection(c_ray ray, c_sphere *sphere)
 {
     double a = 1; //normalized
     double b = (ray.origin.x - sphere->position.x) * ray.direction.x +
-               (ray.origin.y - sphere->position.y) * ray.direction.y +
-               (ray.origin.z - sphere->position.z) * ray.direction.z;
+            (ray.origin.y - sphere->position.y) * ray.direction.y +
+            (ray.origin.z - sphere->position.z) * ray.direction.z;
     b *= 2;
 
-    return 0;
+    double c = pow(ray.origin.x - sphere->position.x, 2) +
+            pow(ray.origin.y - sphere->position.y, 2) +
+            pow(ray.origin.z - sphere->position.z, 2) - pow(sphere->radius, 2);
+
+    double discriminant = b*b - 4*a*c;
+
+    if( discriminant > 0 ){
+        double first_root = (-1*b - sqrt(discriminant))/2*a;
+        double second_root = (-1*b + sqrt(discriminant))/2*a;
+        if(first_root > 0){
+            return first_root;
+        }else{
+            return second_root;
+        }
+    }
+    return -1;
 }
 
